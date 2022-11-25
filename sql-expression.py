e3c2e5cf90d1572341dcc65fa6e2413bbc7ce615
Bastiaan <ba_milo@hotmail.com>
2022-11-25 11:45:01 +0000

    modify

--- sql-expression.py
+++ sql-expression.py
@@ -42,22 +42,25 @@ track_table = Table(
 with db.connect() as connection:
 
     # Query 1 - select all records from the "Artist" table
-    # select_query = artist_table.select()
+    select_query = artist_table.select()
 
     # Query 2 - select only the "Name" column from the "Artist" table
-    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+    # select_query = artist_table.select().with_only_columns(
+    #           [artist_table.c.Name])
 
     # Query 3 - select only "Queen" from the "Artist" table
-    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+    # select_query = artist_table.select().where(
+    #           artist_table.c.Name == "Queen")
 
     # Query 4 - select only by "ArtistId" from the "Artist" table
     # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
 
     # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-    select_query = album_table.select().where(album_table.c.ArtistId == 51)
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
     # Query 6 - select tracks where the composer is "Queen" from "Track" table
-    # cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Santana"])
+    # cursor.execute(
+    #           'SELECT * FROM "Track" WHERE "Composer" = %s', ["Santana"])
 
     results = connection.execute(select_query)
     for result in results:
